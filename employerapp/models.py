c3222af65c9f7f9ff7044431955e3f40a45615e1
llugaliu <129677372+llugaliu@users.noreply.github.com>
2023-07-06 16:03:47 +0200

    Crud App

--- employerapp/models.py
+++ employerapp/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 class Position(models.Model):
     title = models.CharField(max_length=50)
     def __str__(self) -> str:
@@ -10,7 +9,6 @@ class Employer(models.Model):
     phone = models.CharField(max_length=50)
     address = models.CharField(max_length=100,null=True)
     position = models.ForeignKey(Position,on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
